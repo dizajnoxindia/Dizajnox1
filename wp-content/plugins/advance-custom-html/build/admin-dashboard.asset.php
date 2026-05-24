5dc9b85e7e70d78f00894831e8a389e0f70aa4e1
dizajnoxindia <dizajnox@gmail.com>
2026-05-24 19:35:22 +0530

    update

---
+++ wp-content/plugins/advance-custom-html/build/admin-dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '6c1309d68f11b029db9c');
